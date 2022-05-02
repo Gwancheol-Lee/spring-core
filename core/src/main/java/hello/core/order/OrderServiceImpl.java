1d59c33bd238fb94d846174ff8c8ebf78ad5d3b6
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-05-03 00:11:21 +0900

    DIP(추상 객체에 의존), OCP(객체의 의존관계) 위반 상황 발생, 11라인 참고. 인터페이스에만 의존하도록 코드 변경 필요

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,6 +8,9 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 	
 	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+	// private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+	// 위와 같이 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 객체 코드를 변경해야하는 문제 발생. DIP, OCP 위반
 	private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경 
 	
 	// 구현체 클래스인 OrderServiceImpl에서 추상화 클래스인 OrderService의 추상적으로 선언된 createOrder 메소드를 실제 구현한다.
