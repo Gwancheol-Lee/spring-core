d826f48bb6b5ff53cd022a49e1668f72b2137e13
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-05-03 23:49:45 +0900

    추상객체 선언으로 코드 수정 및 생성자를 통해 AppConfig의 참조 레퍼런스 선언.

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 	
-	private final MemberRepository memberRepository = new MemoryMemberRepository(); 
+	private final MemberRepository memberRepository;
+	
+	// 생성자
+	public MemberServiceImpl(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 	
 	// MemberService 인터페이스를 구현한다. 
 	// Override : 부모 클래스(MemberService)의 메서드를 자식클래스(MemberServiceImpl)에서 재 선언 후 사용한다는 걸 명시해주는 어노테이션.  
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,11 +7,14 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 	
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
-	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-	// private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-	// 위와 같이 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 객체 코드를 변경해야하는 문제 발생. DIP, OCP 위반
-	private DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경 
+	private final MemberRepository memberRepository;
+	private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경 
+	
+	// final은 무조건 생성자를 통해서 값 할당
+	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		this.memberRepository = memberRepository;
+		this.discountPolicy = discountPolicy;
+	}
 	
 	// 구현체 클래스인 OrderServiceImpl에서 추상화 클래스인 OrderService의 추상적으로 선언된 createOrder 메소드를 실제 구현한다.
 	@Override
