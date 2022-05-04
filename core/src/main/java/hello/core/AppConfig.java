2a9bb5dae4a4a7003c378a81e9ae7eceb7775598
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-05-04 17:47:55 +0900

    AppConfig 리팩토링 진행. 각 역할과 인터페이스, 구현 클래스의 가독성 향상

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,11 +14,23 @@ public class AppConfig {
 	
 	// 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다
 	
+	// MemberService 인터페이스에 MemberServiceImpl 구현 클래스 주입
 	public MemberService memberService( ) {
-		return new MemberServiceImpl(new MemoryMemberRepository());
+		return new MemberServiceImpl(memberRepository());
 	}
 	
+	// MemberRepository 인터페이스에 MemoryMemberRepository 구현 클래스 주입
+	public MemberRepository memberRepository() {
+		return new MemoryMemberRepository();
+	}
+	
+	// OrderService 인터페이스에 OrderServiceImpl 구현 클래스 주입
 	public OrderService orderService() {
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+		return new OrderServiceImpl(memberRepository(), discountPolicy());
+	}
+	
+	// DiscountPolicy 인터페이스에 FixDiscountPolicy 구현 클래스 주입
+	public DiscountPolicy discountPolicy() {
+		return new FixDiscountPolicy();
 	}
 }
