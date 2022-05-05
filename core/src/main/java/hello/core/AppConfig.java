09d6714a35f3f0bbca8cc839026e7148ada8b7c1
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-05-05 23:35:32 +0900

    소스 최신화

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -10,7 +9,7 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
+// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성 
 public class AppConfig {
 	
 	// 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다
