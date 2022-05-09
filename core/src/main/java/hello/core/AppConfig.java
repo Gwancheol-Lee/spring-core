6fce6cd4219d9eafd111763bd00551edf7b0097f
Gwancheol-Lee <rhkscjf9785@naver.com>
2022-05-09 21:49:02 +0900

    스프링으로 코드 변경. Configuration, Bean 어노테이션 이해. 스프링 컨테이너 동작 이해

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -9,27 +12,32 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-// 애플리케이션의 실제 동작에 필요한 구현 객체를 생성 
+// 애플리케이션 설정 정보 어노테이션
+@Configuration
 public class AppConfig {
 	
 	// 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입(연결) 해준다
 	
-	// MemberService 인터페이스에 MemberServiceImpl 구현 클래스 주입
+	// 스프링 컨테이너에 등록됨
+	@Bean
 	public MemberService memberService( ) {
 		return new MemberServiceImpl(memberRepository());
 	}
 	
 	// MemberRepository 인터페이스에 MemoryMemberRepository 구현 클래스 주입
+	@Bean
 	public MemberRepository memberRepository() {
 		return new MemoryMemberRepository();
 	}
 	
 	// OrderService 인터페이스에 OrderServiceImpl 구현 클래스 주입
+	@Bean
 	public OrderService orderService() {
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 	
 	// DiscountPolicy 인터페이스에 FixDiscountPolicy 구현 클래스 주입
+	@Bean
 	public DiscountPolicy discountPolicy() {
 		// return new FixDiscountPolicy();
 		 return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,18 +1,24 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 	// MemberApp run -> 아래 코드 실행
 	public static void main(String[] args) {
 		
 		// AppConfig 선언
-		AppConfig appConfig = new AppConfig();
-		MemberService memberService = appConfig.memberService(); 
+		// AppConfig appConfig = new AppConfig();
+		// MemberService memberService = appConfig.memberService(); 
 
+		// AppConfig의 환경 설정을 스프링 컨테이너에 빈 등록 
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+		
 		Member member = new Member(1L, "memberA", Grade.VIP); // Member 클래스 선언 및 생성자 변수값 주입
 		memberService.join(member);
 		
